8a0afe0a9bca09f265d6130fb5b325ff33d6c8a7
unknown <Kelcey@Ranook.pixelrebirth.local>
2015-09-06 00:28:01 -0700

    More optimizations using Angular.min instead of angular.js

--- js/Controller.js
+++ js/Controller.js
@@ -2,8 +2,7 @@ angular.module('app', [])
 .controller('Controller', RestController);
 function RestController ($scope, $http) {
     $scope.loadJson = function () {
-	    $http.post('http://web.pixelrebirth.com:8880/api',"module=posh&codeset=dirtree&arg1=c:\\test").
-	    // $http.post('/sample.json').
+	    $http.post('http://web.pixelrebirth.com:8880/api',"module=posh&codeset=dirtree&arg1=c:\\windows\\system32").
 	    success(function(data) {
 	        $scope.markers = data;
 	    }, function(dataError){
