12a6f0b353bc9932786e4c0c686f70f950e8493d
unknown <Kelcey@Ranook.pixelrebirth.local>
2015-09-28 00:27:01 -0700

    ng-Modal working

--- js/MainController.js
+++ js/MainController.js
@@ -1,4 +1,9 @@
-angular.module('app',[])
+angular.module('app',['ngModal'])
+.config(function(ngModalDefaultsProvider) {
+  return ngModalDefaultsProvider.set({
+    closeButtonHtml: "<i class='fa fa-times'></i>"
+  });
+})
 .controller('MainController', Main);
 function Main ($scope) {
 	$scope.SetShowToFalse = function () {
@@ -13,5 +18,20 @@ function Main ($scope) {
 	}
 	$scope.SetMenuItemShow = function ($menuItem) {
 		$scope.$menuItem = true
-	}
+	};
+	$scope.logClose = function() {
+		console.log('close!');
+	};
+	$scope.toggleModal = function() {
+		$scope.myData.modalShown = !$scope.myData.modalShown;
+	};
+	$scope.setFoo = function () {
+		$scope.myData.foo = $scope.myData.hello
+	};
+	$scope.myData = {
+            link: "http://google.com",
+            modalShown: false,
+            hello: 'world',
+            foo: 'bar'
+	};
 };
